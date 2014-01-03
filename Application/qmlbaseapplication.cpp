d2e8a70a2f886b3e757f33864ec9d33d921b366c
Christophe Meurice <meumeu1402@gmail.com>
2014-01-03 10:32:12 +0100

    Initial import

---
+++ Application/qmlbaseapplication.cpp
@@ -0,0 +1,49 @@
+#include "qmlbaseapplication.h"
+
+QmlBaseApplication::QmlBaseApplication(QString qmlfile, QString app, QWidget *parent) : QWidget(parent)
+{
+    qmlRegisterType<SystemVariable>("org.baseapp.jsonstring", 1, 0, "JsonString");
+    qmlRegisterType<Shortcut>("org.baseapp.shortcut", 1, 0, "Shortcut");
+
+    m_qmlFile = qmlfile;
+    m_osInfo = new PlatformInfo(app, this);
+    m_viewer = new QmlDroidViewer();
+    m_context = m_viewer->rootContext();
+
+    m_networkManager = new NetworkManager(m_osInfo->getSetting("serverIp", "127.0.0.1"),this);
+
+    m_model = new VariableModelManager(this);
+        QObject::connect(m_networkManager, SIGNAL(loggedChanged(bool)), m_model, SLOT(setServerAvailable(bool)));
+        QObject::connect(m_networkManager, SIGNAL(systemVariableChanged(QString,QString,QVariant)), m_model, SLOT(updateSystemVariable(QString,QString,QVariant)));
+        QObject::connect(m_model, SIGNAL(requestVariable(QString,QString)), m_networkManager, SLOT(requestVariable(QString,QString)));
+        QObject::connect(m_model, SIGNAL(releaseVariable(QString)), m_networkManager, SLOT(releaseVariable(QString)));
+
+    setContextProperty("variablesModel",m_model);
+    setContextProperty("osInfo",m_osInfo);
+    setContextProperty("network", m_networkManager);
+    setContextProperty("viewer", m_viewer);
+}
+
+void QmlBaseApplication::show(int w,int h){
+
+    m_viewer->setMainQmlFile(m_qmlFile);
+
+    if (m_osInfo->platform() != "Android"){
+        m_viewer->setHeight(h);
+        m_viewer->setWidth(w);
+        m_viewer->show();
+    }
+    else{
+        m_viewer->showExpanded();
+    }
+}
+
+QQmlContext * QmlBaseApplication::getContext(){
+
+    return m_viewer->rootContext();
+}
+
+void QmlBaseApplication::setContextProperty(const QString &name, QObject *value)
+{
+     m_context->setContextProperty(name, value);
+}
---
+++ Application/qmlbaseapplication.h
@@ -0,0 +1,39 @@
+#ifndef QMLBASEAPPLICATION_H
+#define QMLBASEAPPLICATION_H
+
+#include <QObject>
+#include <QWidget>
+#include <QQmlContext>
+
+#include "../QmlViewer/qmldroidviewer.h"
+#include "../NodeCommunication/networkmanager.h"
+#include "../VariableModel/variablemodelmanager.h"
+#include "../VariableModel/systemvariable.h"
+#include "../PlatformInfo/platforminfo.h"
+#include "../ShortCut/shortcut.h"
+
+class QmlBaseApplication : public QWidget
+{
+    Q_OBJECT
+
+    public:
+        explicit QmlBaseApplication(QString qmlfile, QString app, QWidget *parent = 0);
+
+    signals:
+
+    public slots:
+        void show(int w = 1000, int h = 600);
+        QQmlContext *getContext();
+        void setContextProperty(const QString &name, QObject * value);
+
+    private:
+
+        NetworkManager * m_networkManager;
+        VariableModelManager * m_model;
+        PlatformInfo * m_osInfo;
+        QmlDroidViewer * m_viewer;
+        QString m_qmlFile;
+        QQmlContext *m_context;
+};
+
+#endif // QMLBASEAPPLICATION_H
