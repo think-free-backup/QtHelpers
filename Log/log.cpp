082f23c795b95229287dbf3afa3d9a0d96104423
Christophe Meurice <meumeu1402@gmail.com>
2014-05-07 17:12:34 +0200

    Log color

--- Log/log.cpp
+++ Log/log.cpp
@@ -9,5 +9,5 @@ void Log::write(QString function, QString log)
 {
     QDateTime date = QDateTime::currentDateTime();
 
-    qDebug("[%s] : %s (%s)", date.toString("yyyy-MM-dd hh:mm:ss").toStdString().c_str(), log.toStdString().c_str(), function.toStdString().c_str());
+    qDebug("\e[90m[%s] :\e[0m %s (%s)", date.toString("yyyy-MM-dd hh:mm:ss").toStdString().c_str(), log.toStdString().c_str(), function.toStdString().c_str());
 }
