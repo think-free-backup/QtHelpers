2bb2f5be22b6d3d3ea27ee4a697a22e82fc3c631
Christophe <meumeu1402@gmail.com>
2014-06-11 12:09:10 +0200

    Log win

--- Log/log.cpp
+++ Log/log.cpp
@@ -9,7 +9,11 @@ void Log::write(QString function, QString log)
 {
     QDateTime date = QDateTime::currentDateTime();
 
-    qDebug("[%s] : %s (%s)", date.toString("yyyy-MM-dd hh:mm:ss").toStdString().c_str(), log.toStdString().c_str(), function.toStdString().c_str());
+    #ifdef Q_OS_WIN
+        qDebug("[%s] : %s (%s)", date.toString("yyyy-MM-dd hh:mm:ss").toStdString().c_str(), log.toStdString().c_str(), function.toStdString().c_str());
+    #else
+        qDebug("\e[38;5;243m[%s] :\e[0m %s (%s)", date.toString("yyyy-MM-dd hh:mm:ss").toStdString().c_str(), log.toStdString().c_str(), function.toStdString().c_str());
+    #endif
 }
 
 void Log::write_color(QString function, QString log, QString color){
